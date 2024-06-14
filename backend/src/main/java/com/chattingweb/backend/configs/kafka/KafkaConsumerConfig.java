d0f6ebbb7ba09c2d886dfc909e6c238519e8a894
tientu2003 <117565347+tientu2003@users.noreply.github.com>
2024-06-15 00:58:18 +0700

    add env variables

--- backend/src/main/java/com/chattingweb/backend/configs/kafka/KafkaConsumerConfig.java
+++ backend/src/main/java/com/chattingweb/backend/configs/kafka/KafkaConsumerConfig.java
@@ -3,6 +3,7 @@ package com.chattingweb.backend.configs.kafka;
 import com.chattingweb.backend.models.MessageData;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -15,11 +16,18 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConsumerConfig {
+
+    @Value(value = "${kafka.bootstrapAddress}")
+    private String bootstrapAddress;
+
+    @Value(value = "${kafka.groupId}")
+    private String groupId;
+
     @Bean
     public ConsumerFactory<String, MessageData> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "chat");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(props,new StringDeserializer()
