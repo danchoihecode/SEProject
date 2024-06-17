caf39831462091f4d8ce6e2cb102c88bde0dd729
KyHoang0810 <ky.ht210505@sis.hust.edu.vn>
2024-06-17 17:55:28 +0700

    add Restapi for messsage

--- backend/src/main/java/com/chattingweb/backend/services/MessageService.java
+++ backend/src/main/java/com/chattingweb/backend/services/MessageService.java
@@ -1,6 +1,7 @@
 package com.chattingweb.backend.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -23,6 +24,20 @@ public class MessageService {
 		return messages.stream().map(message -> new MessageDTO(message.getId(), message.getMessageContent(),
 				message.getMessageDate(), message.getSender().getId() // Assuming sender has an ID
 		)).collect(Collectors.toList());
+	}
+
+	public void updateMessage(Long messageId, String content){
+		Optional<Message> message=messageRepository.findById(messageId);
+		if(message.isPresent()){
+			Message mess=message.get();
+			mess.setMessageContent(content);
+			messageRepository.save(mess);
+		} else {
+			throw new IllegalArgumentException("Message not found for ID: " + messageId);
+		}
+	}
+	public void deleteMessage(Long messageId){
+		messageRepository.deleteById(messageId);
 	}
 
 }
