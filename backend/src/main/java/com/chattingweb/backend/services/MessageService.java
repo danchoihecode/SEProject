0dcb26412e97b3e026e4dfc3f294ef7181ce88ae
Trinh Dung <dung.tt215187@sis.hust.edu.vn>
2024-06-17 17:54:27 +0700

    Add Read Message

--- backend/src/main/java/com/chattingweb/backend/services/MessageService.java
+++ backend/src/main/java/com/chattingweb/backend/services/MessageService.java
@@ -1,5 +1,6 @@
 package com.chattingweb.backend.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import com.chattingweb.backend.entities.conversation.Message;
 import com.chattingweb.backend.entities.response.MessageDTO;
+import com.chattingweb.backend.models.MessageData;
+import com.chattingweb.backend.models.MessageType;
 import com.chattingweb.backend.repository.conversation.MessageRepository;
 
 @Service
@@ -24,5 +27,19 @@ public class MessageService {
 				message.getMessageDate(), message.getSender().getId() // Assuming sender has an ID
 		)).collect(Collectors.toList());
 	}
-
+	
+	public List<MessageData> getConversationMessages(UUID conversationId) {
+		List<MessageData> messageDataList = new ArrayList<>();
+		for (Message message : messageRepository.findByConversationIdOrderByMessageDateAsc(conversationId)) {
+			MessageData messageData = MessageData.builder()
+//			          .messageType(MessageType.CONNECT)
+			          .messageContent(message.getMessageContent())
+			          .senderUserId(message.getSender().getId())
+			          .conversationId(conversationId)
+//			          .sessionId(message.getSessionId())
+			          .build();
+			messageDataList.add(messageData);
+		}
+	    return messageDataList;
+	  }
 }
