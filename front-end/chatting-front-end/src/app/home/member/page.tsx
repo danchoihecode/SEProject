ea5feb89bb4bba99230c2f78acc9b2dcaa7d6b3c
Duong Hoang Hai <hoanghai2003vp@gmail.com>
2024-07-21 15:03:18 +0700

    Review code

--- front-end/chatting-front-end/src/app/home/member/page.tsx
+++ front-end/chatting-front-end/src/app/home/member/page.tsx
@@ -1,6 +1,6 @@
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
-import { SideBar } from "@/components/side-bar";
+import { Friend, SideBar } from "@/components/side-bar";
 import { SelectedRoomContextProvider } from "@/context/selected-room-context";
 import { Item } from "@/components/item-list";
 import { getConversationList} from "@/server/chatting-data";
@@ -8,15 +8,17 @@ import Members from "@/components/member-list";
 import {getServerSession} from "next-auth/next";
 import {authOption} from "@/configs/next-auth-config";
 import {Session} from "next-auth";
+import {listFriends} from "@/server/add-group";
 
 export default async function MemberPage() {
     const chats: Item[] = await getConversationList();
     const session = await getServerSession(authOption as any) as Session;
+    const friends: Friend[] = await listFriends();
 
     return (
         <Box sx={{ display: 'flex', height: '100%' }}>
             <SelectedRoomContextProvider>
-                <SideBar chats={chats} />
+                <SideBar chats={chats} listFriends={friends} />
                 <Divider orientation="vertical" />
                 <Members userId ={session.id}/>
             </SelectedRoomContextProvider>
