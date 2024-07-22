3a1cdc95139a78e23d2ee220be5514a6bbce4f9c
Duong Hoang Hai <hoanghai2003vp@gmail.com>
2024-07-22 10:37:49 +0700

    Add get profile list

--- backend/src/main/java/com/chattingweb/backend/controller/userprofile/UserProfileController.java
+++ backend/src/main/java/com/chattingweb/backend/controller/userprofile/UserProfileController.java
@@ -1,6 +1,7 @@
 package com.chattingweb.backend.controller.userprofile;
 
 import com.chattingweb.backend.entities.post.Post;
+import com.chattingweb.backend.entities.response.ProfileDTO;
 import com.chattingweb.backend.entities.user.User;
 import com.chattingweb.backend.models.UserProfile;
 import com.chattingweb.backend.repository.post.PostRepository;
@@ -14,31 +15,32 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/userprofile")
+@RequestMapping("/")
 public class UserProfileController {
-    final UserRepository userRepository;
-    private final PostRepository postRepository;
-    public UserProfileController( UserRepository userRepository,PostRepository postRepository) {
-        this.userRepository=userRepository;
-        this.postRepository=postRepository;
-    }
+	final UserRepository userRepository;
+	private final PostRepository postRepository;
 
-    @GetMapping("/")
-    public ResponseEntity<UserProfile> userProfile (@RequestParam(name="id") UUID id, @RequestParam(name="page") Integer page){
-        Optional<User> user= userRepository.findById(id);
-        if(user.isPresent()){
-            List<Post> posts=postRepository.findByUser(user.get(), PageRequest.of(page,10));
-            UserProfile userprofile= new UserProfile();
-            userprofile.setAvatar(user.get().getAvatar());
-            userprofile.setUuid(user.get().getId());
-            userprofile.setNickName(user.get().getNickName());
-            userprofile.setFullName(user.get().getFullName());
-            userprofile.setPostList(posts);
-            return ResponseEntity.ok(userprofile);
+	public UserProfileController(UserRepository userRepository, PostRepository postRepository) {
+		this.userRepository = userRepository;
+		this.postRepository = postRepository;
+	}
 
-        }
-        return ResponseEntity.notFound().build();
-    }
+	@GetMapping("/userprofile")
+	public ResponseEntity<ProfileDTO> userProfile(@RequestParam(name = "id") UUID id) {
+		System.out.println(id.toString());
+		Optional<User> user = userRepository.findById(id);
+		if (user.isPresent()) {
+			try {
+				List<Post> posts = postRepository.findByUser(user.get());
+				return ResponseEntity.ok(new ProfileDTO(user.get().getId(), user.get().getNickName(),
+						user.get().getFullName(), user.get().getAvatar(), posts));
+			} catch (Exception e) {
+				e.printStackTrace();
+				return ResponseEntity.notFound().build();
+			}
 
+		}
+		return ResponseEntity.notFound().build();
+	}
 
 }
--- backend/src/main/java/com/chattingweb/backend/entities/post/Post.java
+++ backend/src/main/java/com/chattingweb/backend/entities/post/Post.java
@@ -36,7 +36,7 @@ public class Post {
     private Integer noLike;
 
     @NotNull
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
